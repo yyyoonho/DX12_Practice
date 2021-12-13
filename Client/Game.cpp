a11e93a88acc962742a41b65c415d317f40e0e4f
yyyoonho <parn997@naver.com>
2021-12-14 01:04:09 +0900

    feat: Input, Timer 추가

---
+++ Client/Game.cpp
@@ -0,0 +1,91 @@
+#include "pch.h"
+#include "Game.h"
+#include "Engine.h"
+
+shared_ptr<Mesh> mesh = make_shared<Mesh>();
+shared_ptr<Shader> shader = make_shared<Shader>();
+shared_ptr<Texture> texture = make_shared<Texture>();
+
+void Game::Init(const WindowInfo& info)
+{
+	GEngine->Init(info);
+
+	vector<Vertex> vec(4);
+	vec[0].pos = Vec3(-0.5f, 0.5f, 0.5f);
+	vec[0].color = Vec4(1.f, 0.f, 0.f, 1.f);
+	vec[0].uv = Vec2(0.f, 0.f);
+	vec[1].pos = Vec3(0.5f, 0.5f, 0.5f);
+	vec[1].color = Vec4(0.f, 1.f, 0.f, 1.f);
+	vec[1].uv = Vec2(1.f, 0.f);
+	vec[2].pos = Vec3(0.5f, -0.5f, 0.5f);
+	vec[2].color = Vec4(0.f, 0.f, 1.f, 1.f);
+	vec[2].uv = Vec2(1.f, 1.f);
+	vec[3].pos = Vec3(-0.5f, -0.5f, 0.5f);
+	vec[3].color = Vec4(0.f, 1.f, 0.f, 1.f);
+	vec[3].uv = Vec2(0.f, 1.f);
+
+	vector<uint32> indexVec;
+	{
+		indexVec.push_back(0);
+		indexVec.push_back(1);
+		indexVec.push_back(2);
+	}
+	{
+		indexVec.push_back(0);
+		indexVec.push_back(2);
+		indexVec.push_back(3);
+	}
+
+	mesh->Init(vec, indexVec);
+
+	shader->Init(L"..\\Resources\\Shader\\default.hlsli");
+
+	texture->Init(L"..\\Resources\\Texture\\veigar.jpg");
+
+	GEngine->GetCmdQueue()->WaitSync();
+}
+
+void Game::Update()
+{
+	GEngine->Update();
+
+	GEngine->RenderBegin();
+
+	shader->Update();
+
+
+	{
+		static Transform t = {};
+		
+		if (INPUT->GetButton(KEY_TYPE::W))
+			t.offset.y += 1.f * DELTA_TIME;
+		if (INPUT->GetButton(KEY_TYPE::S))
+			t.offset.y -= 1.f * DELTA_TIME;
+		if (INPUT->GetButton(KEY_TYPE::A))
+			t.offset.x -= 1.f * DELTA_TIME;
+		if (INPUT->GetButton(KEY_TYPE::D))
+			t.offset.x += 1.f * DELTA_TIME;
+
+
+		mesh->SetTransform(t);
+
+		mesh->SetTexture(texture);
+
+		mesh->Render();
+	}
+
+
+	/*{
+		Transform t;
+		t.offset = Vec4(0.f, 0.f, 0.f, 0.f);
+		mesh->SetTransform(t);
+
+		mesh->SetTexture(texture);
+
+		mesh->Render();
+	}*/
+
+
+
+	GEngine->RenderEnd();
+}
