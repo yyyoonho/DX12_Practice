82b8f118a5c6bd2be103b5e6cce76df63f409ae2
yyyoonho <parn997@naver.com>
2022-03-08 15:53:39 +0900

    feat: Normal Mapping/ Tangent Space

--- Engine/SceneManager.cpp
+++ Engine/SceneManager.cpp
@@ -55,7 +55,7 @@ shared_ptr<Scene> SceneManager::LoadTestScene()
 	scene->AddGameObject(camera);
 #pragma endregion
 
-#pragma region Sphere
+#pragma region Cube
 	{
 		shared_ptr<GameObject> sphere = make_shared<GameObject>();
 		sphere->AddComponent(make_shared<Transform>());
@@ -63,17 +63,20 @@ shared_ptr<Scene> SceneManager::LoadTestScene()
 		sphere->GetTransform()->SetLocalPosition(Vec3(0.f, 0.f, 150.f));
 		shared_ptr<MeshRenderer> meshRenderer = make_shared<MeshRenderer>();
 		{
-			shared_ptr<Mesh> sphereMesh = GET_SINGLE(Resources)->LoadSphereMesh();
+			shared_ptr<Mesh> sphereMesh = GET_SINGLE(Resources)->LoadCubeMesh();
 			meshRenderer->SetMesh(sphereMesh);
 		}
 		{
 			shared_ptr<Shader> shader = make_shared<Shader>();
 			shared_ptr<Texture> texture = make_shared<Texture>();
+			shared_ptr<Texture> texture2 = make_shared<Texture>();
 			shader->Init(L"..\\Resources\\Shader\\default.hlsli");
-			texture->Init(L"..\\Resources\\Texture\\veigar.jpg");
+			texture->Init(L"..\\Resources\\Texture\\Hose.jpg");
+			texture2->Init(L"..\\Resources\\Texture\\Hose_Normal.jpg");
 			shared_ptr<Material> material = make_shared<Material>();
 			material->SetShader(shader);
 			material->SetTexture(0, texture);
+			material->SetTexture(1, texture2);
 			meshRenderer->SetMaterial(material);
 		}
 		sphere->AddComponent(meshRenderer);
@@ -87,10 +90,10 @@ shared_ptr<Scene> SceneManager::LoadTestScene()
 		light->AddComponent(make_shared<Transform>());
 		//light->GetTransform()->SetLocalPosition(Vec3(0.f, 150.f, 150.f));
 		light->AddComponent(make_shared<Light>());
-		light->GetLight()->SetLightDirection(Vec3(0.f, -1.f, 0.f));
+		light->GetLight()->SetLightDirection(Vec3(1.f, 0.f, 1.f));
 		light->GetLight()->SetLightType(LIGHT_TYPE::DIRECTIONAL_LIGHT);
-		light->GetLight()->SetDiffuse(Vec3(0.1f, 1.f, 0.1f));
-		light->GetLight()->SetAmbient(Vec3(0.f, 0.1f, 0.f));
+		light->GetLight()->SetDiffuse(Vec3(0.5f, 0.5f, 0.5f));
+		light->GetLight()->SetAmbient(Vec3(0.1f, 0.1f, 0.1f));
 		light->GetLight()->SetSpecular(Vec3(0.1f, 0.1f, 0.1f));
 
 		scene->AddGameObject(light);
@@ -98,6 +101,7 @@ shared_ptr<Scene> SceneManager::LoadTestScene()
 
 #pragma endregion
 
+/*
 #pragma region Red Point Light
 	{
 		shared_ptr<GameObject> light = make_shared<GameObject>();
@@ -131,6 +135,6 @@ shared_ptr<Scene> SceneManager::LoadTestScene()
 		scene->AddGameObject(light);
 	}
 #pragma endregion
-
+*/
 	return scene;
 }
